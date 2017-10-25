2cbbb1cf40c26387df10ce84ec4fb5d6df3d72ed
Sara Kalo <sara.kalo@studenti.unitn.it>
2017-10-25 14:44:48 +0200

    prog con lato prof funzionante

--- server.js
+++ server.js
@@ -1,16 +1,74 @@
+
+//http server
 var express = require('express');
 var app=express();
 var http=require('http').Server(app);
+
+//socket io
 var io=require('socket.io')(http);
 
+//files access
+var fs=require('fs');
+
+//tree generator
+var parser=require(__dirname+'/parser.js');
+var tregen=require(__dirname+'/treeGenesis.js');
+
+//======================================================================
+
+
 var ids_cell=[];
-	
+
+
+var amount=require(__dirname+'/clients/trees/number_trees.json').amount;
+console.log(parseInt(Math.random()*amount));
+console.log(parseInt(Math.random()*amount));
+console.log(parseInt(Math.random()*amount));
+var tree=require(__dirname+'/clients/trees/tree'+/*parseInt(Math.random()*amount)*/2+'.json');
 app.use(express.static(__dirname+'/clients'));
 
 
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/clients/html/index.html');
 });
+
+
+app.get('/addexpressions', function(req, res){
+res.sendFile(__dirname + '/clients/html/addexpressions.html');
+});
+
+app.get('/sendexp', function(req, res){
+	var amount;
+	/*fs.readFileSync(__dirname+"/clients/trees/number_trees.txt","utf8",function(err,data){
+		amount=parseInt(data);
+		if(err){console.log(err);}
+		console.log("!!!!");
+		console.log(amount);
+		console.log(data);
+		console.log("!!!!!");
+	});*/
+	//var amount=require(__dirname+'/clients/trees/number_trees.json').amount;
+	var  amount =JSON.parse(fs.readFileSync(__dirname+'/clients/trees/number_trees.json')).amount;
+	console.log(req.query.expression);
+	var ris=(parser.parse(req.query.expression));
+	var tree=tregen.TreeGenesis(ris);
+		console.log("!!!!");
+		console.log(amount);
+		console.log("!!!!!");
+	fs.writeFile(__dirname+"/clients/trees/tree"+amount+".json",tree,function(err){
+		if(!err){fs.writeFile(__dirname+"/clients/trees/number_trees.json",'{"amount": '+(parseInt(amount)+1)+"}",function(err){
+			if(err){console.log(err);}
+			});
+		}
+		else{console.log(err);}
+		
+	});
+
+	console.log(tregen.TreeGenesis(ris));
+	
+});
+
+
 
 var id_n = 0;
 
@@ -20,6 +78,7 @@ var create_socket = function(socket)
 	id_n++;
 	
 	console.log('user ' + id + ' connected');
+	socket.emit('sendtree',tree);
 	socket.emit('arraysender',ids_cell);
 	socket.on('disconnect', function()
 		{
---
+++ treeGenesis.js
@@ -0,0 +1,46 @@
+module.exports={TreeGenesis};
+
+function TreeGenesis(postString){
+	var i=postString.length-1;
+	var id=0;
+	function readNext(){
+		var str='';
+		var val=postString[i];
+		i--;
+		if(val.type=='Operator'){
+			var left=readNext();
+			var right=readNext();
+
+			var ris;
+			switch(val.value){
+				case '+': ris=left.value+right.value;break;
+				case '-': ris=left.value-right.value;break;
+				case '*': ris=left.value*right.value;break;
+				case '/': ris=left.value/right.value;break;
+				case '^': ris=Math.pow(left.value,right.value);break;
+			}
+			str+='{\n';
+			str+='"innerHTML": "'+val.value+"<br/><input id='"+(id++)+"' type='text' onkeydown='check(event)' data-result='"+ris+"'"+'/>",\n';
+			str+='"children": [\n';
+			str+=left.string + ",\n";
+			str+=right.string;
+			str+="]\n";
+			str+='}\n';
+			return{string:str,value:ris};
+		}
+		else{
+			str+='{"text": { "name": '+val.value+' }}\n';
+			return{string:str,value:parseFloat(val.value)};
+		}
+	};
+
+	return '{'+
+    '"chart": {'+
+        '"container": "#tree-simple",'+
+		'"rootOrientation": "SOUTH"'+
+    '},'+
+    
+    '"nodeStructure": '+
+	readNext().string+ '}';
+
+};
